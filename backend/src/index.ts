1f1ef449e325f0c02c4c6a7b5108b30a72b7e103
jp9141joao <jp9141joao@gmail.com>
2025-02-25 00:49:53 -0300

    commit

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,10 @@ import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://rick-and-morty-api-back-end.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
