b8614b0806e8abe4c39d714b2766c9257f714787
jp9141joao <jp9141joao@gmail.com>
2025-02-25 01:19:22 -0300

    commit

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -8,13 +8,25 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+const allowedOrigins = [
+    'https://rick-and-morty-api-jp9141joao.netlify.app',
+    'https://outro-dominio.com'
+];
 app.use((0, cors_1.default)({
-    origin: 'https://rick-and-morty-api-back-end.onrender.com',
+    origin: function (origin, callback) {
+        if (!origin)
+            return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'A política CORS do servidor não permite acesso a partir desta origem.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express_1.default.json());
 app.use(routes_1.routes);
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,11 +2,25 @@ import express from 'express';
 import cors from 'cors';
 import { routes } from './routes';
 
+
 const app = express();
 const port = process.env.PORT || 3000;
+const allowedOrigins = [
+  'https://rick-and-morty-api-jp9141joao.netlify.app',
+  'https://outro-dominio.com'
+];
 
 app.use(cors({
-  origin: 'https://rick-and-morty-api-back-end.onrender.com',
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'A política CORS do servidor não permite acesso a partir desta origem.';
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  },
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
